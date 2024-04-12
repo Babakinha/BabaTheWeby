0bdacad5021ce40cb186e12c1cb34f7880b22351
Babakinha <59146844+Babakinha@users.noreply.github.com>
2024-04-11 22:40:19 -0300

    Organize stuff and make some lighthouse improvments

--- src/home.rs
+++ src/home.rs
@@ -3,26 +3,24 @@ use sycamore::prelude::*;
 
 fn home_page<G: Html>(cx: Scope) -> View<G> {
     view! { cx,
-        div {
-            h1 { "Hewwo wowld :3" }
-            p {
-                "Haii :3, I'm Baba, just your average nerdy "
-                i { "(social anxiety and depression filled)" }
-                " Femboi! ^^"
-            }
-            p {
-                "I didnt really think to what i should put here yet... "
-                i { "(and tbh im just kinda lazy to do it)" }
-            }
-            p {
-                "but... i will prob put some cool stuff here... "
-                i { "someday..." }
-            }
-            br {}
-            p { "so uhm... i guess thats it, hope u have a nice dayy :3" }
-            div(style="height: 1000px")
-            p { "I love you ❤️" }
+        h1 { "Hewwo wowld :3" }
+        p {
+            "Haii :3, I'm Baba, just your average nerdy "
+            i { "(social anxiety and depression filled)" }
+            " Femboi! ^^"
         }
+        p {
+            "I didnt really think to what i should put here yet... "
+            i { "(and tbh im just kinda lazy to do it)" }
+        }
+        p {
+            "but... i will prob put some cool stuff here... "
+            i { "someday..." }
+        }
+        br {}
+        p { "so uhm... i guess thats it, hope u have a nice dayy :3" }
+        div(style="height: 1000px")
+        p { "I love you ❤️" }
     }
 }
 
@@ -31,6 +29,7 @@ fn head(cx: Scope) -> View<SsrNode> {
     view! { cx,
         title { "OwO What is this?" }
 
+        meta(name="description", content="Haii :3, I'm Baba, just your average nerdy femboy\nWelcome to my website :3")
         //TODO: maybe change the .perseus thing somehow
         link(href="/.perseus/static/assets/baba.css", rel="stylesheet")
 
